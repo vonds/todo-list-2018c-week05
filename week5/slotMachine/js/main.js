e9d89e5e2a7d692de82e97ae68a4b101cff39d40
Vonds <vonds13@gmail.com>
2018-10-13 09:56:47 -0400

    added some sudo/comments

--- week5/slotMachine/js/main.js
+++ week5/slotMachine/js/main.js
@@ -1,55 +1,63 @@
 //Reviewing this I'm realizing that I need to add notes and sudo code. I will make another commit with both
 
-var Coins = 50;
+// Create an amount that the user would start off with
+var coins = 50;
+// var slotMain = document.getElementsByClassName('play');
 
-document.getElementById("coinID").innerHTML = "Total Coins = " + Coins;
-function myFunction() {
+// Create a total amount 
+document.getElementById("coinId").innerHTML = "Total Coins = " + coins;
+
+function play() {
+  // Create parameters for random numbers
   var x = Math.floor((Math.random() * 5) + 1);
   var y = Math.floor((Math.random() * 5) + 1);
   var z = Math.floor((Math.random() * 5) + 1);
-  var bid = document.getElementById("BidID").value;
-  if (bid > 0 && bid < Coins) {
-    document.getElementById("demo").innerHTML = x;
-    document.getElementById("demo2").innerHTML = y;
-    document.getElementById("demo3").innerHTML = z;
+  // Create variable bid
+  var bid = document.getElementById("bidId").value;
+  // If the is more than 0 and less than coins then...
+  if (bid > 0 && bid < coins) {
+    // render the values for x, y, and z on screen
+    document.getElementById("num").innerHTML = x;
+    document.getElementById("num2").innerHTML = y;
+    document.getElementById("num3").innerHTML = z;
 
+    // A conditional to check to see if there are matches
+    // If x is equal to y or x is equal to z or y is equal to z...
     if (x == y || x == z || y == z) {
-      var bid = document.getElementById("BidID").value;
-      document.getElementById("res").innerHTML = "Alright my g";
-      Coins = Coins + bid * 2;
-      document.getElementById("coinID").innerHTML = "Total Coins = " + Coins;
+      //then grab the value from the input with the "bidId"
+      var bid = document.getElementById("bidId").value;
+      // Send a message that notifies the user that they won
+      document.getElementById("smMessge").innerHTML = "Alright my g";
+      // Take the bid variable(dynamic value from the input), multiply the value by 2, and add it to the variable coins
+      coins = coins + bid * 2;
+      // Update the on screen amount for total coins
+      document.getElementById("coinId").innerHTML = "Total Coins = " + coins;
+      // Update the on screen amount for total coins
       document.getElementById("payout").innerHTML = "Won " + bid * 2 + " coins!!!";
     }
     else {
-      var bid = document.getElementById("BidID").value;
-      document.getElementById("res").innerHTML = "You a bum";
-      Coins = Coins - bid;
-      document.getElementById("coinID").innerHTML = "Total Coins = " + Coins;
+      // else if none are true...
+      // grab the value from the input with the "bidId"
+      var bid = document.getElementById("bidId").value;
+      // Send a message that notifies the user that they lost
+      document.getElementById("smMessge").innerHTML = "You a bum";
+      // Take the bid variable(dynamic value from the input) and subtract it to the variable coins
+      coins = coins - bid;
+      // Update the on screen amount for total coins
+      document.getElementById("coinId").innerHTML = "Total Coins = " + coins;
+      // Update the on screen amount for total coins
       document.getElementById("payout").innerHTML = "lost " + bid + " coins";
     }
 
   }
+  // Else in the situation where bid is less than 0 and more than coins available......
   else {
-    document.getElementById("res").innerHTML = "you have to bid more then 0 - . -; or less then max coins";
+    // then render the following message on the screen using the paragraph with the class "smMessage"
+    document.getElementById("smMessge").innerHTML = "you have to bid more then 0 or less then your max coins";
   }
 }
 
-function win() {
-  var bid = document.getElementById("BidID").value;
-  document.getElementById("res").innerHTML = "Alright my g";
-  Coins = Coins + bid * 2;
-  document.getElementById("coinID").innerHTML = "Total Coins = " + Coins;
-  document.getElementById("payout").innerHTML = "Won " + bid * 2 + " coins!!!";
-}
 
-function lose() {
-  var bid = document.getElementById("BidID").value;
-  document.getElementById("res").innerHTML = "You a bum";
-  Coins = Coins - bid;
-  document.getElementById("coinID").innerHTML = "Total Coins = " + Coins;
-  document.getElementById("payout").innerHTML = "lost " + bid + " coins";
-
-}
 
 
 
